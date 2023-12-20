ccc8c4a891146fb8084c37c5433eab9f5217dc83
FewJuho <a.popovprodby@gmail.com>
2023-12-20 04:18:46 +0300

    added enemy anims

--- Assets/Scripts/Utils/Enemy.cs
+++ Assets/Scripts/Utils/Enemy.cs
@@ -1,11 +1,15 @@
+using GLTF.Schema;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     public int healthPoints = 100; // TODO: different enemies?
 
+    private bool isdead = false;
+    public Animator animator;
     public float speed = 2.0f;
     public int hitDamage = 250;
     public GameObject ammoPrefab;
@@ -19,21 +23,43 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
+        if (isdead)
+        {
+            return;
+        }
         Vector3 lookAt = _player.transform.position;
         lookAt.y = transform.position.y;
         transform.LookAt(lookAt);
 
         transform.position += transform.forward * speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, _player.transform.position) < 5.0f)
+        {
+            animator.SetTrigger("BiteTrigger");
+        }
     }
 
     public void ReactToDamage(int damage) {
+        if (isdead)
+        {
+            return;
+        }
         healthPoints -= damage * (int)DataHolder.damageMultiplier;
+        animator.SetTrigger("HitTrigger");
         if (healthPoints <= 0) {
+            isdead = true;
             // TODO: add animation before nonexistence
             ++DataHolder.killedEnemiesCount;
             Debug.Log("Kill " + DataHolder.killedEnemiesCount.ToString());
             Instantiate(ammoPrefab, this.transform.position,  Quaternion.identity);
-            Destroy(this.transform.gameObject);
+            animator.SetBool("Dies", true);
+            StartCoroutine(DeathAnimation());
         }
+    }
+
+    IEnumerator DeathAnimation()
+    {
+        yield return new WaitForSeconds(2.0f);
+        Destroy(this.transform.gameObject);
     }
 }
